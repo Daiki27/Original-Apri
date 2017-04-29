30a587c25a4a4975600c0ed62232c30736b33615
樋口大樹 <daikiit27@gmail.com>
2017-04-29 17:27:51 +0900

    難しい

--- original apri/AddViewController.swift
+++ original apri/AddViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import RealmSwift
 
 class AddViewController: UIViewController {
+    //ToDoItemに色々入ってる。パッケージ。objectをToDoItemに継承する。objectに色々入ってる。
+    class ToDoItem: Object{
+        //名前
+        dynamic var name = "to do"
+        //期限
+        //dynamic var deadLine = NSDate(timeIntervalSince1970: 0)
+        //完了フラグ
+        dynamic var isComplete = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +34,18 @@ class AddViewController: UIViewController {
     @IBAction func modoru(){
         self.dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var nameLabel: UILabel!
+//    
+//    @IBAction func add(_ sender: Any) {
+//        self.performSegue(withIdentifier: "add", sender: nil)
+//    }
+//    
+    @IBOutlet weak var text: UITextField!
     
+    @IBOutlet weak var todonametext: UITextField!
+    
+//    @IBAction func addtodo(_ sender: Any) {
+//    }
     /*
     // MARK: - Navigation
 
